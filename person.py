983e83daeccf9d19ef59707898c583555a1198cf
Samuel Folledo <samuelfolledo@gmail.com>
2019-10-03 19:57:28 -0700

    [add] Person's test_sick_person_instantiation method. [add] Person's infection = to infection

--- person.py
+++ person.py
@@ -17,7 +17,7 @@ class Person(object):
         self._id = _id # int
         self.is_alive = True  # boolean
         self.is_vaccinated = is_vaccinated # boolean
-        self.infection = None  # Virus object or None
+        self.infection = infection  # Virus object or None
 
     def did_survive_infection(self): #method that returns True if Person's immunity is higher than virus's mortality rate
         if self.infection == None:
@@ -62,11 +62,23 @@ def test_not_vacc_person_instantiation():
 def test_sick_person_instantiation():
     virus = Virus("Dysentery", 0.7, 0.2)
     person = Person(3, False, virus) #person with the virus
-    # TODO: complete your own assert statements that test
-    # the values at each attribute
-    # assert ...
-    pass
+    assert person._id == 3
+    assert person.is_alive is True
+    assert person.is_vaccinated is False
+    assert person.infection is virus
+    assert person.infection.name is "Dysentery"
+    assert person.infection.repro_rate is 0.7
+    assert person.infection.mortality_rate is 0.2
 
+    hiv = Virus("HIV", 0.8, 0.3)
+    kid = Person(123, False, hiv)
+    assert kid._id == 123
+    assert kid.is_alive is True
+    assert kid.is_vaccinated is False
+    assert kid.infection is hiv
+    assert kid.infection.name == "HIV"
+    assert kid.infection.repro_rate is 0.8
+    assert kid.infection.mortality_rate is 0.3
 
 def test_did_survive_infection():
     # TODO: Create a Virus object to give a Person object an infection
@@ -94,3 +106,4 @@ def test_did_survive_infection():
 if __name__ == "__main__":
     test_vacc_person_instantiation()
     test_not_vacc_person_instantiation()
+    test_sick_person_instantiation()
